8a4a897eb9cbd9204488df5a69d31a87c59edaf2
kkh1178 <kalisha.hill@gmail.com>
2018-08-22 23:32:49 -0500

    Added a gradient that will make the circles look like planets

--- animation/app3.js
+++ animation/app3.js
@@ -8,16 +8,51 @@ var svg = d3.select("#planetarium").insert("svg")
 
 // svg.append("circle").attr("r", 20).attr("cx", w / 2)
 //     .attr("cy", h / 2).attr("class", "sun")
+svg.html(`<defs>
+<radialGradient id="exampleGradient"  cx="20%" cy="50%" r="60%" fx="50%" fy="50%">
+  <stop offset="0%" stop-color="silver"/>
+  <stop offset="100%" stop-color="blue"/>
+</radialGradient>
+</defs>`)
+
+svg.html(`<defs>
+<radialGradient id="exampleGradient"  cx="20%" cy="50%" r="60%" fx="50%" fy="50%">
+  <stop offset="0%" stop-color="silver"/>
+  <stop offset="100%" stop-color="blue"/>
+</radialGradient>
+</defs>`)
+
+svg.html(`<defs>
+<radialGradient id="exampleGradient"  cx="20%" cy="50%" r="60%" fx="50%" fy="50%">
+  <stop offset="0%" stop-color="silver"/>
+  <stop offset="100%" stop-color="blue"/>
+</radialGradient>
+</defs>`)
+
+svg.html(`<defs>
+<radialGradient id="exampleGradient"  cx="20%" cy="50%" r="60%" fx="50%" fy="50%">
+  <stop offset="0%" stop-color="silver"/>
+  <stop offset="100%" stop-color="blue"/>
+</radialGradient>
+</defs>`)
+
+svg.html(`<defs>
+<radialGradient id="exampleGradient"  cx="20%" cy="50%" r="60%" fx="50%" fy="50%">
+  <stop offset="0%" stop-color="silver"/>
+  <stop offset="100%" stop-color="blue"/>
+</radialGradient>
+</defs>`)
 
 //  Adding an image of the sun to act as our star
 svg.append("svg:image")
-    .attr("x", -10 + w / 2)
-    .attr("y", -10 + h / 2)
+    .attr("x", -20 + w / 2)
+    .attr("y", -20 + h / 2)
     .attr("class", "sun")
     .attr("xlink:href", "sun.png")
-    .attr("width", 20)
-    .attr("height", 20)
+    .attr("width", 40)
+    .attr("height", 40)
     .attr("text-anchor", "middle");
+
 
 // 
 var container = svg.append("g")
@@ -41,7 +76,7 @@ d3.csv("cleaned_planets.csv").then(function (data) {
 
         // I'm using a column from the table called  Radial Velocity [km/s]
 
-        d.Orbital_Velocity_proportional = +d.Orbital_Velocity_proportional / 2;
+        d.Orbital_Velocity_proportional = +d.Orbital_Velocity_proportional / 5;
 
         d.Effective_temperature_K = +d.Effective_temperature_K;
     });
@@ -71,9 +106,9 @@ d3.csv("cleaned_planets.csv").then(function (data) {
                 .attr("r", d.Planet_Radius_Earth_Radii)
                 .attr("cx", d.Semi_Major_Axis_proportional)
                 .attr("cy", 0)
-                
+
                 .attr("class", planetClass)
-                
+
                 .attr("id", "planet-body");
         });
 
@@ -93,9 +128,10 @@ d3.csv("cleaned_planets.csv").then(function (data) {
                 axis = datum.Semi_Major_Axis_proportional
                 toolTip.style("visibility", "visible");
                 toolTip.select('.tooltip-container').html(`Planet Name: <strong>${datum.Planet_Name}</strong>`)
+                // toolTip.select(".orbit").attr("stroke-opacity", 1)
                 // toolTip.select(".radius_planet").html(`Planet Radii: <strong>${datum.Planet_Radius_Earth_Radii}</strong>`)
             })
-            .on("mouseout", function(datum){
+            .on("mouseout", function (datum) {
                 // toolTip.style("visibility", "hidden");
                 // toolTip.select("#planetarium")
             })
@@ -106,7 +142,7 @@ d3.csv("cleaned_planets.csv").then(function (data) {
     });
 });
 
-svg.on("click", function() {
+svg.on("click", function () {
     toolTip.style("visibility", "hidden");
 })
 
@@ -116,11 +152,12 @@ function toRad(deg) {
 }
 
 var stellarClass = [
-    { "sClassName": "B [10,000 - 30,000 K]", "planetClass": "stellarB", "color": "#10E3F4", "y": 20},
-    { "sClassName": "A [7,500 - 10,000 K]", "planetClass": "stellarA", "color": "#CEF8FC", "y": 40},
+    { "sClassName": "B [10,000 - 30,000 K]", "planetClass": "stellarB", "color": "#10E3F4", "y": 20 },
+    { "sClassName": "A [7,500 - 10,000 K]", "planetClass": "stellarA", "color": "#CEF8FC", "y": 40 },
     { "sClassName": "F [6,000 - 7,500 K]", "planetClass": "stellarF", "color": "#F9FCA9", "y": 60 },
     { "sClassName": "G [5,000 - 6,000 K]", "planetClass": "stellarG", "color": "#EDF410", "y": 80 },
-    { "sClassName": "K [3,500 - 5,000 K]", "planetClass": "stellarK", "color": "#F4AB10", "y": 100 }
+    { "sClassName": "K [3,500 - 5,000 K]", "planetClass": "stellarK", "color": "#F4AB10", "y": 100 },
+    { "sClassName": "M [0 - 3,500 K]", "planetClass": "stellarM", "color": "#F4AB10", "y": 120 }
 
 ];
 
@@ -132,13 +169,15 @@ container.append("g").selectAll("text").data(stellarClass).enter().append("text"
     .attr("font-size", "20px")
     // .position("right")
     .attr("x", 350)
-    .attr("y", d => 20+d.y)
-    .attr("font-family", "sans-serif")
-    .attr("fill", "black")
+    .attr("y", d => 20 + d.y)
+    .attr("font-family", "Oswald")
+    .attr("fill", "#9CF1F4")
+    // .text("Orbiting Star Heat (K)")
+    // .attr("text-align", center)
     .on("mouseover", function () {
         var selectedClass = d3.select(this).attr("class");
 
-        d3.selectAll("circle").style("opacity", 0.3).classed("selected-planets", false);
+        d3.selectAll("circle").style("opacity", 0.2).classed("selected-planets", false);
         d3.selectAll("." + selectedClass).style("opacity", 1).classed("selected-planets", true);
     })
     .on("mouseout", function () {
