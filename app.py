b448e05f3cfb5fd2d9a3ef28aad9954aba5acd83
Schrodinger <35243347+cburdon@users.noreply.github.com>
2018-08-23 20:09:50 -0500

    added API functionality to flask app

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
-from flask import Flask, render_template, jsonify, redirect
+from flask import Flask, render_template, jsonify, redirect, request
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
+import pandas as pd
+import csv
+
 #from ipynbfiledconverted import the function
 
 app = Flask(__name__)
-conn = 'mongodb://localhost:27017'
-client = MongoClient(conn)
-db = client.planets_db
+app.config["MONGO_URI"] = "mongodb://localhost:27017/planets_db"
+mongo = PyMongo(app)
+mongo.db.planets_db.drop()
+info = pd.read_csv("cleaned_planets.csv", index_col=False).drop("Unnamed: 0", axis=1)
+info_json = info.to_dict(orient='records')
+mongo.db.planets_db.insert_many(info_json)
 
 @app.route("/")
 def index():
@@ -19,8 +26,10 @@ def bubble():
 def distance():
     return render_template("distance_earth.html")
 
-# @app.route("/data")
-# def data():
+@app.route("/data")
+def data():
+    planet_data = list(mongo.db.planets_db.find({}, {"_id": 0 }))
+    return jsonify(planet_data)
 
 
 if __name__ == "__main__":
