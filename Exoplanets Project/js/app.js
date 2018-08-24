3d6cc7b4ba6e08df42c3751facbf3ac8238612bd
John Singson <john.singson9@gmail.com>
2018-08-23 20:35:53 -0500

    bubble and 3rd visual

--- Exoplanets Project/js/app.js
+++ Exoplanets Project/js/app.js
@@ -12,7 +12,7 @@ function makeResponsive() {
 
     // Define the SVG Area
     var svgWidth = (window.innerWidth)*.7;
-    var svgHeight = (window.innerHeight)*.8;
+    var svgHeight = (window.innerHeight);
 
     // Define chart margins
     var margin = {
@@ -27,7 +27,7 @@ function makeResponsive() {
     var chartHeight = svgHeight - margin.top - margin.bottom;
 
     // Select scatter to append SVG area and set dimensions
-    var svg = d3.select("#scatter")
+    var svg = d3.select("#planet-distance")
         .append("svg")
         .attr("width", svgWidth)
         .attr("height", svgHeight);
@@ -37,8 +37,8 @@ function makeResponsive() {
         .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
     // Load data from data.csv
-    var stateFile = "https://raw.githubusercontent.com/the-Coding-Boot-Camp-at-UT/UTAUS201804DATA2-Class-Repository-DATA/master/16-D3/HOMEWORK/Instructions/data/data.csv?token=Aiw9AiBgR6LiTrmpQBMNqgvQuUI5PZDkks5bdcGdwA%3D%3D";
-    d3.csv(stateFile).then(successHandle, errorHandle);
+    var file = "https://raw.githubusercontent.com/cburdon/Exoplanets/master/cleaned_planets.csv";
+    d3.csv(file).then(successHandle, errorHandle);
 
     // Throw error if it occurs
     function errorHandle(error) {
@@ -46,35 +46,25 @@ function makeResponsive() {
     }
 
     // If successful
-    function successHandle(stateData) {
+    function successHandle(planetData) {
 
         // Print the data
-        console.log(stateData);
+        console.log(planetData);
 
         // Cast the integer data as numbers
-        stateData.forEach(function(data) {
-            data.age = +data.age;
-            data.healthcare = +data.healthcare;
-            data.healthcareHigh = +data.healthcareHigh;
-            data.healthcareLow = +data.healthcareLow;
-            data.income = +data.income;
-            data.obesity = +data.obesity;
-            data.obesityHigh = +data.obesityHigh;
-            data.obesityLow = +data.obesityLow;
-            data.poverty = +data.poverty;
-            data.smokes = +data.smokes;
-            data.smokesHigh = +data.smokesHigh;
-            data.smokesLow = +data.smokesLow;
-        });
+        planetData.forEach(function(data) {
+            data.minimum_Travel_Time_yr = +data.minimum_Travel_Time_yr;
+            data.Planet_Radius_Earth_Radii = +data.Planet_Radius_Earth_Radii;
+        }); 
 
         // X Scale
         var xScale = d3.scaleLinear()
-            .domain([(d3.min(stateData, data => data.poverty)-1), (d3.max(stateData, data => data.poverty)+1)])
+            .domain([(d3.min(planetData, data => data.minimum_Travel_Time_yr-25000000)), (d3.max(planetData, data => data.minimum_Travel_Time_yr))])
             .range([0, chartWidth]);
 
         // Y Scale
         var yScale = d3.scaleLinear()
-            .domain([(d3.min(stateData, data => data.obesity)-1), (d3.max(stateData, data => data.obesity)+1)])
+            .domain([(d3.min(planetData, data => data.Planet_Radius_Earth_Radii-1)), (d3.max(planetData, data => data.Planet_Radius_Earth_Radii))])
             .range([chartHeight, 0]);
 
         // Create Axes
@@ -93,33 +83,55 @@ function makeResponsive() {
         chartGroup.append("text")
             .attr("text-anchor", "middle")
             .attr("transform", `translate(${chartWidth / 2}, ${chartHeight + 40})`)
-            .text("Poverty by State");
+            .text("Minimum Travel Time");
 
         chartGroup.append("text")
             .attr("text-anchor", "middle")
             .attr("transform", "rotate(-90)")
             .attr("x", 0 - (chartHeight/2))
             .attr("y", 50 - margin.left)
-            .text("Obesity by State");
+            .text("Planet Radius");
 
         // Append circles to datapoints
         var circlesGroup = chartGroup.selectAll("circle")
-            .data(stateData)
+            .data(planetData)
             .enter()
             .append("circle")
-            .attr("cx", data => xScale(data.poverty))
-            .attr("cy", data => yScale(data.obesity))
+            .attr("cx", data => xScale(data.minimum_Travel_Time_yr))
+            .attr("cy", data => yScale(data.Planet_Radius_Earth_Radii))
             .attr("r", "15")
             .attr("stroke-width", "1")
-            .attr("class", "stateCircle");
+            .attr("class", "planetCircle");
 
+        var toolTip3 = d3.select("#planet-distance")
+            .append("div")
+            .style("display", "none")
+            .attr("class", "toolTip3");
+
+        circlesGroup.on("mouseover", function(d) {
+            toolTip3.style("display", "block")
+                .html(
+                    `<strong>${d.Planet_Name}</strong>
+                    <hr>
+                    ${d.Distance_to_Star_ly}`
+                )
+                .style("left", d3.event.pageX + "px")
+                .style("top", d3.event.pageY + "px");
+        })
+            .on("mouseout", function() {
+                toolTip3.style("display", "none");
+            });
         // Append names to the datapoints
-        chartGroup.append("g")
-            .selectAll("text")
-            .data(stateData)
-            .enter()
-            .append("text")
-            .text(d => d.abbr)
-            .attr("dx", d => xScale(d.poverty))
-            .attr("dy", d => yScale(d.obesity)+5)
-            .attr("class", "stateText");
+        // chartGroup.append("g")
+        //     .selectAll("text")
+        //     .data(planetData)
+        //     .enter()
+        //     .append("text")
+        //     .text(d => d.abbr)
+        //     .attr("dx", d => xScale(d.Distance_to_Star_ly))
+        //     .attr("dy", d => yScale(d.minimum_Travel_Time_yr))
+        //     .attr("class", "planetText");
+
+    }
+
+}
--- Exoplanets Project/js/app2.js
+++ Exoplanets Project/js/app2.js
@@ -19,38 +19,51 @@ var svg = d3.select("#planet-bubble")
     .attr("width", width)
     .attr("height", height);
 
-var numNodes = 100;
-var nodes = d3.range(numNodes).map(function(d) {
-  return {radius: Math.random() * 50}
-});
+// var numNodes = 100;
+// var nodes = d3.range(numNodes).map(function(d) {
+//   return {radius: Math.random() * 50}
+// });
 
-console.log(nodes);
+var toolTip = d3.select("#planet-bubble")
+                .append("div")
+                .attr("class", "toolTip");
 
-// Forced Center Bubble Chart Code Steps
-    // Step 1: Create the array of objects
-        // Use d3 to call the nodes from csv or json
-        // d3.csv("exoplanets.csv", function(data) {
-        //     data.radius = +data.radius;
 
- var simulation = d3.forceSimulation(nodes)
-        .force('charge', d3.forceManyBody().strength(50))
+var file = "https://raw.githubusercontent.com/cburdon/Exoplanets/master/cleaned_planets.csv";
+d3.csv(file).then(successHandle, errorHandle);
+
+function errorHandle(error){
+    return error;
+}
+
+function successHandle(planetdata) {
+
+    planetdata.forEach(function(data) {
+        data.Planet_Radius_km = +data.Planet_Radius_km;
+        // console.log(nodes);
+        data.Effective_temperature_K = +data.Effective_temperature_K;
+    })
+
+    console.log(planetdata);
+
+    var simulation = d3.forceSimulation()
+       .force("collide", d3.forceCollide( function(d){return markerSize(d.Planet_Radius_km) + 1 }).iterations(24) )
+        .force('charge', d3.forceManyBody().strength(5))
         .force('center', d3.forceCenter(width / 2, height / 2))
-        .force('collision', d3.forceCollide().radius(function(d) {
-            return d.radius + 1
-        }))
-        .on('tick', ticked);
+        .force("x", d3.forceX(0))
+        .force("y", d3.forceY(0));
+
+    simulation.nodes(planetdata)
+        .on("tick", ticked);
 
     function ticked() {
         var node = d3.select('svg')
             .selectAll('circle')
-            .data(nodes)
-            
+            .data(planetdata)  
 
         node.enter()
             .append('circle')
-            .attr('r', function(d) {
-                return d.radius
-            })
+            .attr('r', d => markerSize(d.Planet_Radius_km))
             .merge(node)
             .attr('cx', function(d) {
                 return d.x
@@ -58,27 +71,68 @@ console.log(nodes);
             .attr('cy', function(d) {
                 return d.y
             })
-            .style("fill", function(d) {
-                return color(d.radius)
+            .attr("fill", function(d, i) {
+                return "url(#grad" + i + ")";
             })
+            // .style("fill", d => color(d.Effective_temperature_K))
             .call(d3.drag()
                 .on("start", dragstart)
                 .on("drag", drag)
                 .on("end", dragend));
 
-        var toolTip = d3.select("body")
-            .append("div")
-            .attr("class", "toolTip");
+        var grads = svg.append("defs").selectAll("radialGradient")
+            .data(planetdata)
+            .enter()
+            .append("radialGradient")
+            .attr("gradientUnits", "objectBoudingBox")
+            .attr("cx", 0)
+            .attr("cy", 0)
+            .attr("r", "100%")
+            .attr("id", function(d, i) {return "grad" + i;});
+
+        grads.append("stop")
+            .attr("offset", "0%")
+            .style("stop-color", function(d) {return color(d.Effective_temperature_K); })
+
+        grads.append("stop")
+            .attr("offset", "100%")
+            .style("stop-color", function(d) {return color2(d.Effective_temperature_K); })
+
+        // var legend = svg.append("g")
+        //                 .attr("class", "legend")
+        //                 .attr("x", -width)
+        //                 .attr("y", -height)
+        //                 .attr("height", 200)
+        //                 .attr("width", 100);
+
+        // legend.append("rect")
+        //       .attr("x", -width)
+        //       .attr("y", -height)
+        //       .attr("width", 10)
+        //       .attr("height", 10)
+        //       .style("fill", function(d) { return color2(d.Effective_temperature_K)});
+
+        // var legendcolor = { 0 : ["B: 10000-30000", "#451804"],
+        //                     1 : ["A: 7500-10000", "#451804"],
+        //                     2 : ["F: 6000-7500", "#451804"],
+        //                     3 : ["G: 5000-6000", "#451804"],
+        //                     4 : ["K: 3500-5000", "#451804"],
+        //                     5 : ["M: 0-3500", "#451804"],
+
+        // legend.append("text")
+        //       .attr("x", -width)
+        //       .attr("y", -height)
+        //       .text(function(d) { return })
         
         node.on("mouseover", function(d) {
             toolTip.style("display", "block")
-                .html(`<p>${d.radius}</p>`)
+                .html(`<p>${d.Planet_Name}</p>`)
                 .style("left", d3.event.pageX + "px")
                 .style("top", d3.event.pageY + "px");
         })
-            .on("mouseout", function() {
-                toolTip.style("display", "none");
-            });
+        .on("mouseout", function() {
+            toolTip.style("display", "none");
+        });
 
         node.exit().remove();
     }
@@ -103,16 +157,47 @@ console.log(nodes);
     function color(Kelvin) {
         
         if (Kelvin > 10000) {
-            return "blue";
+            return "#c1440e";
+        }
+        if (Kelvin > 7500) {
+            return "#e77d11";
         }
         if (Kelvin > 6000) {
-            return "white";
+            return "#d7c797";
         }
-        if (Kelvin > 3700) {
-            return "yellow";
+        if (Kelvin > 5000) {
+            return "#9fc164";
         }
-        return "red";
-
+        if (Kelvin > 3500) {
+            return "#6b93d6";
+        }
+        return "#7e8173";
     }
+
+    function color2(Kelvin) {
+        
+        if (Kelvin > 10000) {
+            return "#451804";
+        }
+        if (Kelvin > 7500) {
+            return "c1440e";
+        }
+        if (Kelvin > 6000) {
+            return "#845422";
+        }
+        if (Kelvin > 5000) {
+            return "#6b93d6";
+        }
+        if (Kelvin > 3500) {
+            return "#4f4cb0";
+        }
+        return "#361d1d";
+    }
+
+    function markerSize(radius) {
+        return radius/3700;
+    }
+
+}
 
 }
